aa69e91163da50301ffff56bda8b256b2df56151
hans <hmrtn@pm.me>
2022-01-18 11:04:15 -0800

    updated link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ const Home = () => {
             Distribute tokens among your team members.{" "}
           </Text>
           <Box maxW="300px" pt="10">
-            <Link isExternal href="https://pay-party-dev.herokuapp.com/">
+            <Link isExternal href="https://app.pay.party">
               <Button w="239px" h="51px" fontSize="xl">
                 Launch App
               </Button>
